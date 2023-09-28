899e5d5ad772186e2684e122a3f5e9d9ecd16544
sundarrkshan <sundarrkshan@gmail.com>
2023-09-28 19:30:52 +0530

    Stream API- Flat Map-IntStream Map to Int Example

---
+++ src/main/java/org/tutorial/StreamLimitElemnts.java
@@ -0,0 +1,15 @@
+package org.tutorial;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamLimitElemnts {
+    public static void main(String[] args) {
+        Stream<Integer> IntStream = Stream.iterate(0,n->n+2);
+
+        List<Integer> listOf10 = IntStream.filter(s->s%2==0).limit(10).collect(Collectors.toList());
+
+        System.out.println("First 10 elemnts in the List"+listOf10);
+    }
+}
